dd10d1a998d84b008e20472950c28b7e9100bb5f
timw <tim.webster9@protonmail.com>
2022-07-03 21:29:11 +0100

    update

---
+++ build.ps1
@@ -0,0 +1,46 @@
+$rev = "0.1"
+$revSuffix="_v$rev"
+
+$devMissionName = 'dmx'
+$missionName = 'inplainsight'
+$missionDir = "C:\games\darkmod\fms\$devMissionName"
+$stagingDir = 'C:\Temp\dm_staging'
+$stagingMissionDir = "$stagingDir\$devMissionName"
+$darkmodtxt = "$stagingMissionDir\darkmod.txt"
+
+$pkg = "$stagingMissionDir\$missionName$revSuffix"
+
+$dmap = Read-Host -Prompt 'Did you delete and recompile the map files?'
+if ( $dmap -notin "Y","y") {
+    exit 1
+}
+
+$playerStart = Read-Host -Prompt 'Did you reset the player start position?'
+if ( $playerStart -notin "Y","y") {
+    exit 1
+}
+
+$masterKey = Read-Host -Prompt 'Did you disable the master key?'
+if ( $masterKey -notin "Y","y") {
+    exit 1
+}
+
+# clean staging directory and copy latest code
+remove-item -path $stagingDir\* -Filter * -Force -Recurse
+copy-item -path $missionDir -destination $stagingDir -recurse
+
+# remove unwanted files
+remove-item -path $stagingMissionDir -include .git,savegames,.gitignore,build.ps1,changelog.txt,consolehistory.dat,dmx.darkradiant,dmx.bak,dmx.darkradiant.bak,dmx.xd.bkup,story.md -Force -Recurse
+
+# token replace version
+(Get-Content $darkmodtxt).replace('[VERSION]', $rev) | Set-Content $darkmodtxt 
+
+# compress and rename main pk4
+$compress = @{
+    Path = "$stagingMissionDir\*"
+    CompressionLevel = "Optimal"
+    DestinationPath = "$pkg.zip"
+}
+Compress-Archive @compress
+rename-item -path "$pkg.zip" -newname "$pkg.pk4"
+
